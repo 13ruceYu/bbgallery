18293e06124742eeb7342c919bbeaeee7798a381
13ruceYu <bruceyuhongbo@gmail.com>
2024-07-18 17:33:13 +0800

    chore: build improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  'https://dummyimage.com/800x600/000213/d23233',
+  'https://dummyimage.com/800x600/100213/a23233',
+  'https://dummyimage.com/800x600/200213/b23233',
+  'https://dummyimage.com/800x600/300213/c23233',
+]
+
+const mockImages = mockUrls.map((url, index) => (
+  {
+    id: index + 1,
+    url,
+  }
+))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      hello gallery in progress
+    <main className="flex flex-wrap gap-4 px-4">
+      {mockImages.map(({ id, url }) => (
+        <div key={id} className="w-48">
+          <img src={url} />
+        </div>
+      ))}
     </main>
   );
 }
